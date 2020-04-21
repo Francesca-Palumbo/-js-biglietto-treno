adb85dc2ca3244ff52934a7dc342de8b23f1a2c9
Francesca Palumbo <econofra87@gmail.com>
2020-04-21 10:08:33 +0200

    maiuscole

--- java/java.js
+++ java/java.js
@@ -5,23 +5,23 @@
 // va applicato uno sconto del 40% per gli over 65
 
 var eta_passeggero = Math.floor(Math.random() * 100);
-    console.log( 'il passeggero ha' + eta_passeggero + 'anni' );
+    console.log( 'Il passeggero ha' + eta_passeggero + 'anni' );
 
 var km_percorrere = Math.floor(Math.random() * 1000);
-    console.log( 'il passeggero deve percorrere: ' + km_percorrere + ' km');
+    console.log( 'Il passeggero deve percorrere: ' + km_percorrere + ' km');
 
 // prezzo biglietto
 var prezzo_biglietto = ( ( km_percorrere ) *0.21 );
-    console.log( 'il prezzo del biglieto del passeggero è di €:' + prezzo_biglietto );
+    console.log( 'Il prezzo del biglieto del passeggero è di €:' + prezzo_biglietto );
 
 // prezzo biglietto under 18
 var sconto_giovani = ( prezzo_biglietto - ( prezzo_biglietto ) *0.2 );
     if ( eta_passeggero < 18 ) {
-        console.log( 'il prezzo del biglieto del passeggere sarebbe stato di €:' + prezzo_biglietto + '.' + 'Ma con lo sconto è di €:' + sconto_giovani );
+        console.log( 'Ma con lo sconto è di €:' + sconto_giovani );
 }
 
 // prezzo biglietto over 65
 var sconto_anziani = ( prezzo_biglietto - ( prezzo_biglietto ) *0.4 );
     if ( eta_passeggero > 65 ) {
-        console.log( 'il prezzo del biglieto del passeggere sarebbe stato di €:' + prezzo_biglietto + '.' + 'Ma con lo sconto è di €:' + sconto_anziani );
+        console.log( 'Ma con lo sconto è di €:' + sconto_anziani );
 }
